0f72399f62664981e75b2e352ba4b0fc01a9e270
dannyjanapon-lang <dannyjanapon@sjp2cd.edu.ph>
2025-09-08 11:39:39 +0800

    init final

--- app/src/main/java/com/example/mycalculated/MainActivity.kt
+++ app/src/main/java/com/example/mycalculated/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mycalcute_lator.ui.theme.MyCALCUTELatorTheme
+import com.example.mycalculated.ui.theme.MycalculatedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyCALCUTELatorTheme {
+            MycalculatedTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyCALCUTELatorTheme {
+    MycalculatedTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/mycalculated/Mycalculatedjava.java
+++ app/src/main/java/com/example/mycalculated/Mycalculatedjava.java
@@ -15,7 +15,7 @@ public class Mycalculatedjava extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout);
 
         // Initialize UI components
         setupUIComponents();
