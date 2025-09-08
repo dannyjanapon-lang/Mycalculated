233ea4564de4fd29c58126cac6e26cc75694b37b
dannyjanapon-lang <dannyjanapon@sjp2cd.edu.ph>
2025-09-08 11:24:28 +0800

    init

--- app/src/main/java/com/example/mycalculated/MainActivity.kt
+++ app/src/main/java/com/example/mycalculated/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mycalculated
+package com.example.mycalcute_lator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mycalculated.ui.theme.MycalculatedTheme
+import com.example.mycalcute_lator.ui.theme.MyCALCUTELatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MycalculatedTheme {
+            MyCALCUTELatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MycalculatedTheme {
+    MyCALCUTELatorTheme {
         Greeting("Android")
     }
 }
